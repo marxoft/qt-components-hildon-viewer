8f712a6d99e26f148593491d8c7947df0241b5e0
Stuart Howarth <showarth@marxoft.co.uk>
2017-04-10 18:54:00 +0100

    Use first argument as the QML file. Connect QDeclarativeEngine::quit() signal to QApplication::quit() slot.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
-/*
+/*!
  * Copyright (C) 2014 Stuart Howarth <showarth@marxoft.co.uk>
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU Lesser General Public License,
+ * under the terms and conditions of the GNU General Public License,
  * version 3, as published by the Free Software Foundation.
  *
  * This program is distributed in the hope it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  */
@@ -26,17 +26,18 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     app.setOrganizationName("QtComponentsHildon");
-    app.setApplicationName(QObject::tr("Viewer"));
+    app.setApplicationName("Viewer");
     
     QStringList args = app.arguments();
     QString qmlFile;
     
     if (args.size() > 1) {
-        qmlFile = args.last();
+        qmlFile = args.at(1);
     }
     
     if (qmlFile.isEmpty()) {
-        qmlFile = QFileDialog::getOpenFileName(0, QObject::tr("Open QML file"), "/home/user/", QObject::tr("QML files (*.qml)"));
+        qmlFile = QFileDialog::getOpenFileName(0, QObject::tr("Open QML file"), "/home/user/",
+                QObject::tr("QML files (*.qml)"));
     }
     
     if (qmlFile.isEmpty()) {
@@ -49,12 +50,13 @@ int main(int argc, char *argv[]) {
     component.create();
 
     if (component.isError()) {
-        foreach (QDeclarativeError error, component.errors()) {
+        foreach (const QDeclarativeError &error, component.errors()) {
             qWarning() << error.toString();
         }
 
         return 0;
     }
 
+    QObject::connect(&engine, SIGNAL(quit()), &app, SLOT(quit()));
     return app.exec();
 }
